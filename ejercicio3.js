7d2c5dbc2194b4d8a9360e4d85ec426afc320f80
karen cruz <karenabigailcruz16@gmail.com>
2026-06-16 21:42:26 -0300

    'ejer3'

---
+++ ejercicio3.js
@@ -0,0 +1,12 @@
+/**
+ * Crea un bucle for que recorra un array de productos.
+ *Dentro del bucle, utiliza console.log() para imprimir un 
+ *mensaje por cada elemento. El mensaje debe indicar lo que necesitas
+ *comprar, siguiendo el formato: "¡No te olvides de comprar [producto]!".
+ */
+
+const lista= ['leche', 'pan', 'gaseosa', 'queso', 'pure de tomate']
+
+for(let i = 0 ; i < lista.length ; i++){
+    console.log('No te olvides de comprar ' + lista[i] + '!')
+}
